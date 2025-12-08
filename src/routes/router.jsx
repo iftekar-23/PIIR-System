9089c67089281c5f7c5bb2e4bf386806f6ffa2a4
iftekar-23 <aiftekar798@gmail.com>
2025-12-08 10:40:58 +0600

    added rootlayout

--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,8 +1,15 @@
 import { createBrowserRouter } from "react-router";
+import RootLayout from "../layouts/RootLayout";
 
 export const router = createBrowserRouter([
     {
         path:'/',
-        
+        element: <RootLayout></RootLayout>,
+        children:([
+            {
+                index:true,
+                
+            }
+        ])
     }
 ])
