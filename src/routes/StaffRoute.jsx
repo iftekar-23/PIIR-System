cef8f9d20a54b7fe054f4a51f5de88274151699d
iftekar-23 <aiftekar798@gmail.com>
2025-12-17 07:48:53 +0600

    added some change in ui

--- src/routes/StaffRoute.jsx
+++ src/routes/StaffRoute.jsx
@@ -1,20 +1,20 @@
-// import { Navigate } from "react-router";
-// import useRole from "../hooks/useRole";
-// import useAuth from "../hooks/useAuth";
-// import LoadingSpinner from "../components/LoadingSpinner";
+import { Navigate } from "react-router";
+import useRole from "../hooks/useRole";
+import useAuth from "../hooks/useAuth";
+import LoadingSpinner from "../components/LoadingSpinner";
 
-// const StaffRoute = ({ children }) => {
-//   const { user, loading: authLoading } = useAuth();
-//   const { role, loading: roleLoading } = useRole();
+const StaffRoute = ({ children }) => {
+  const { user, loading: authLoading } = useAuth();
+  const { role, loading: roleLoading } = useRole();
 
-//   if (authLoading || roleLoading) {
-//     return <LoadingSpinner />;
-//   }
+  if (authLoading || roleLoading) {
+    return <LoadingSpinner />;
+  }
 
-//   if (!user) return <Navigate to="/login" replace />;
-//   if (role !== "staff") return <Navigate to="/" replace />;
+  if (!user) return <Navigate to="/login" replace />;
+  if (role !== "staff") return <Navigate to="/" replace />;
 
-//   return children;
-// };
+  return children;
+};
 
-// export default StaffRoute;
+export default StaffRoute;
